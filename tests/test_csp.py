8111edd68d7cb134aea5cef78a97437745c69fab
Nils Quetschlich <Nils.Quetschlich@tum.de>
2023-02-10 07:44:10 +0100

    ✨ 🚨 added ruff and adjusted the code accordingly

--- tests/test_csp.py
+++ tests/test_csp.py
@@ -7,9 +7,7 @@ def test_csp():
     sum_s1 = 3
     sum_s2 = 3
     sum_s3 = 1
-    list_of_constraints = csp.get_kakuro_constraints(
-        sum_s0=sum_s0, sum_s1=sum_s1, sum_s2=sum_s2, sum_s3=sum_s3
-    )
+    list_of_constraints = csp.get_kakuro_constraints(sum_s0=sum_s0, sum_s1=sum_s1, sum_s2=sum_s2, sum_s3=sum_s3)
     res = csp.solve(constraints=list_of_constraints, quantum_algorithm="Grover")
     assert res == (0, 3, 1, 0)
 
@@ -17,8 +15,6 @@ def test_csp():
     sum_s1 = 5
     sum_s2 = 3
     sum_s3 = 5
-    list_of_constraints = csp.get_kakuro_constraints(
-        sum_s0=sum_s0, sum_s1=sum_s1, sum_s2=sum_s2, sum_s3=sum_s3
-    )
+    list_of_constraints = csp.get_kakuro_constraints(sum_s0=sum_s0, sum_s1=sum_s1, sum_s2=sum_s2, sum_s3=sum_s3)
     res = csp.solve(constraints=list_of_constraints, quantum_algorithm="Grover")
     assert res == (0, 3, 3, 2)
