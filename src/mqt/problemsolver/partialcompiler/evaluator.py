f74dc40257f24311ad3b4dd523e3edc5225b0507
Nils Quetschlich <Nils.Quetschlich@tum.de>
2023-03-21 16:04:25 +0100

    🎨 added further parameter to evaluation

--- src/mqt/problemsolver/partialcompiler/evaluator.py
+++ src/mqt/problemsolver/partialcompiler/evaluator.py
@@ -3,18 +3,21 @@ from time import time
 # from mqt.qcec import verify
 from typing import TypedDict
 
-from mqt.problemsolver.partialcompiler.qaoa import QAOA, check_gates
+from mqt.problemsolver.partialcompiler.qaoa import QAOA
 
 
 # @dataclass
 class Result(TypedDict):
-    time_ratio: float
-    cx_count_ratio: float
     num_qubits: int
     num_repetitions: int
     sample_probability: float
-    time_new_scheme: float
     time_baseline: float
+    time_new_scheme_without_swap_opt: float
+    time_new_scheme_with_swap_opt: float
+    time_ratio_without_swap_opt: float
+    time_ratio_with_swap_opt: float
+    cx_count_ratio_without_swap_opt: float
+    cx_count_ratio_with_swap_opt: float
     opt_level_baseline: int
 
 
@@ -22,41 +25,51 @@ def evaluate_QAOA(
     num_qubits: int = 4,
     repetitions: int = 3,
     sample_probability: float = 0.5,
-    optimize_swaps: bool = False,
     opt_level_baseline: int = 2,
 ) -> Result:
     q = QAOA(num_qubits=num_qubits, repetitions=repetitions, sample_probability=sample_probability)
 
     qc_compiled_with_all_gates = q.qc_compiled.copy()
     start = time()
-    compiled_qc = check_gates(
+    compiled_qc_without_swap_opt = q.check_gates(
         qc=qc_compiled_with_all_gates,
-        remove_gates=q.remove_gates,
-        to_be_checked_gates_indices=q.to_be_checked_gates_indices,
-        optimize_swaps=optimize_swaps,
+        optimize_swaps=False,
     )
-    # if optimize_swaps:
-    #     compiled_qc = reduce_swaps(qc=q.qc_compiled)
-    time_new_scheme = time() - start
-    # add a print function which prints the average processing time per element in q.remove_gates
+    time_new_scheme_without_swap_opt = time() - start
+
+    qc_compiled_with_all_gates = q.qc_compiled.copy()
+    start = time()
+    compiled_qc_with_swap_opt = q.check_gates(
+        qc=qc_compiled_with_all_gates,
+        optimize_swaps=True,
+    )
+    time_new_scheme_with_swap_opt = time() - start
 
     start = time()
     qc_baseline_compiled = q.compile_qc(baseline=True, opt_level=opt_level_baseline)
     time_baseline = time() - start
 
-    time_ratio = time_new_scheme / time_baseline
-    cx_count_ratio = compiled_qc.count_ops()["cx"] / qc_baseline_compiled.count_ops()["cx"]
+    time_ratio_without_swap_opt = time_new_scheme_without_swap_opt / time_baseline
+    cx_count_ratio_without_swap_opt = (
+        compiled_qc_without_swap_opt.count_ops()["cx"] / qc_baseline_compiled.count_ops()["cx"]
+    )
+    time_ratio_with_swap_opt = time_new_scheme_with_swap_opt / time_baseline
+    cx_count_ratio_with_swap_opt = compiled_qc_with_swap_opt.count_ops()["cx"] / qc_baseline_compiled.count_ops()["cx"]
+
     # try:
     #     print("QCEC:", verify(compiled_qc, qc_baseline_compiled))
     # except:
     #     print("QCEC: False")
     return Result(
-        time_ratio=time_ratio,
-        cx_count_ratio=cx_count_ratio,
         num_qubits=num_qubits,
         num_repetitions=repetitions,
         sample_probability=sample_probability,
-        time_new_scheme=time_new_scheme,
         time_baseline=time_baseline,
+        time_ratio_without_swap_opt=time_ratio_without_swap_opt,
+        time_ratio_with_swap_opt=time_ratio_with_swap_opt,
+        time_new_scheme_without_swap_opt=time_new_scheme_without_swap_opt,
+        time_new_scheme_with_swap_opt=time_new_scheme_with_swap_opt,
+        cx_count_ratio_without_swap_opt=cx_count_ratio_without_swap_opt,
+        cx_count_ratio_with_swap_opt=cx_count_ratio_with_swap_opt,
         opt_level_baseline=opt_level_baseline,
     )
