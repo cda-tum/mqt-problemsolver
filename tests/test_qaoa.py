aa9b27e9fae1868ce16c43c0077fd9065c6fd3de
Nils Quetschlich <Nils.Quetschlich@tum.de>
2023-03-21 13:44:41 +0100

    ✅ added tests for qaoa

--- tests/test_qaoa.py
+++ tests/test_qaoa.py
@@ -1,6 +1,6 @@
-from mqt.problemsolver.partialcompiler.qaoa import QAOA
+from mqt.problemsolver.partialcompiler.qaoa import QAOA, check_gates
 from qiskit import QuantumCircuit
-from typing import List
+
 
 def test_qaoa_init() -> None:
     q = QAOA(num_qubits=4, repetitions=3, sample_probability=0.5)
@@ -9,9 +9,28 @@ def test_qaoa_init() -> None:
     assert isinstance(q.qc_compiled, QuantumCircuit)
     assert isinstance(q.to_be_checked_gates_indices, list)
 
+
 def test_compile_qc() -> None:
     q = QAOA(num_qubits=4, repetitions=3, sample_probability=0.5)
     qc_baseline_compiled = q.compile_qc(baseline=True, opt_level=2)
     assert isinstance(qc_baseline_compiled, QuantumCircuit)
     qc_baseline_compiled = q.compile_qc(baseline=False, opt_level=2)
     assert isinstance(qc_baseline_compiled, QuantumCircuit)
+
+
+def test_get_to_be_checked_gates() -> None:
+    q = QAOA(num_qubits=4, repetitions=3, sample_probability=0.5)
+    indices = q.get_to_be_checked_gates()
+    assert isinstance(indices, list)
+
+
+def test_check_gates() -> None:
+    q = QAOA(num_qubits=2, repetitions=1, sample_probability=1.0)
+    compiled_qc = check_gates(
+        qc=q.qc_compiled.copy(),
+        remove_gates=q.remove_gates,
+        to_be_checked_gates_indices=q.to_be_checked_gates_indices,
+        optimize_swaps=False,
+    )
+    assert isinstance(compiled_qc, QuantumCircuit)
+    assert len(compiled_qc._data) == len(q.qc_compiled._data) - 1
