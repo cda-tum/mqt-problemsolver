eb3d39d9ce9e639a001d3514f6fba76b98dec13d
Nils Quetschlich <Nils.Quetschlich@tum.de>
2023-03-28 13:53:27 +0200

    🎨 code readability improvements

--- src/mqt/problemsolver/partialcompiler/qaoa.py
+++ src/mqt/problemsolver/partialcompiler/qaoa.py
@@ -3,15 +3,7 @@ from __future__ import annotations
 import numpy as np
 from qiskit import QuantumCircuit, transpile
 from qiskit.circuit import Parameter
-from qiskit.providers.fake_provider import FakeManila, FakeMontreal, FakeWashington
-from qiskit.transpiler import PassManager
-from qiskit.transpiler.passes import (
-    Collect2qBlocks,
-    CommutativeCancellation,
-    CommutativeInverseCancellation,
-    ConsolidateBlocks,
-    CXCancellation,
-)
+from qiskit.providers.fake_provider import FakeBackend, FakeManila, FakeMontreal, FakeWashington
 
 
 class QAOA:
@@ -24,59 +16,57 @@ class QAOA:
     ):
         self.num_qubits = num_qubits
         self.repetitions = repetitions
+
         assert 0 <= remove_probability <= 1
         self.sample_probability = remove_probability
         np.random.seed(42)
-        FakeManila().configuration()
-        montreal_config = FakeMontreal().configuration()
-        washington_config = FakeWashington().configuration()
-        manila_config = FakeManila().configuration()
-        FakeManila().configuration()
-        if num_qubits <= manila_config.n_qubits:
-            self.backend = FakeManila()
-        elif num_qubits <= montreal_config.n_qubits:
-            self.backend = FakeMontreal()
-        elif num_qubits <= washington_config.n_qubits:
-            self.backend = FakeWashington()
+
+        self.backend = get_backend(num_qubits)
 
         qc, qc_baseline, remove_gates = self.get_uncompiled_circuits(considered_following_qubits)
         self.qc = qc  # QC with all gates
         self.qc_baseline = qc_baseline  # QC with only the sampled gates
         self.remove_gates = remove_gates  # List of booleans indicating whether a gate should be removed
-        self.qc_compiled = self.compile_qc(baseline=False, opt_level=2)  # Compiled QC with all gates
-        self.to_be_checked_gates_indices = self.get_to_be_removed_gate_indices()  # Indices of the gates to be checked
+        self.qc_compiled = self.compile_qc(baseline=False, opt_level=3)  # Compiled QC with all gates
+        self.to_be_removed_gates_indices = self.get_to_be_removed_gate_indices()  # Indices of the gates to be checked
 
     def get_uncompiled_circuits(
         self, considered_following_qubits: int
     ) -> tuple[QuantumCircuit, QuantumCircuit, list[bool | str]]:
-        qc = QuantumCircuit(self.num_qubits)
-        qc_baseline = QuantumCircuit(self.num_qubits)
+        """Returns the uncompiled circuits (both with only the actual needed two-qubit gates and with all possible
+        two-qubit gates) and the list of gates to be removed."""
+
+        qc = QuantumCircuit(self.num_qubits)  # QC with all gates
+        qc_baseline = QuantumCircuit(self.num_qubits)  # QC with only the sampled gates
         qc.h(range(self.num_qubits))
         qc_baseline.h(range(self.num_qubits))
 
         remove_gates: list[bool | str] = []
         parameter_counter = 0
         tmp_len = -1
+
+        # Iterate over all QAOA layers
         for k in range(self.repetitions):
             if k == 1:
-                tmp_len = len(remove_gates)
+                tmp_len = len(remove_gates)  # Number of parameterized gates in the first layer
+
+            # Iterate over all possible two-qubit gates
             for i in range(self.num_qubits):
-                for j in range(
-                    i + 1, i + 1 + considered_following_qubits
-                ):  # i + 1 + considered_following_qubits): #min(self.num_qubits, )
+                # considered_following_qubits describes the number of to be considered following qubits
+                for j in range(i + 1, i + 1 + considered_following_qubits):
                     if j >= self.num_qubits:
                         break
                     p = Parameter(f"a_{parameter_counter}")
                     qc.rzz(p, i, j)
+                    # Sample whether the gate should be removed for the first layer
                     if k == 0:
                         if np.random.random() < self.sample_probability:
                             remove_gates.append(p.name)
                         else:
                             remove_gates.append(False)
                             qc_baseline.rzz(p, i, j)
-                    elif remove_gates[
-                        parameter_counter - k * tmp_len
-                    ]:  # parameter_counter%(self.num_qubits * (self.num_qubits - 1)//2)
+                    # For all other layers, check whether the gate should be removed
+                    elif remove_gates[parameter_counter - k * tmp_len]:
                         remove_gates.append(p.name)
                     else:
                         remove_gates.append(False)
@@ -84,6 +74,8 @@ class QAOA:
                     parameter_counter += 1
 
             m = Parameter(f"b_{k}")
+
+            # Mixer Layer
             qc.rx(2 * m, range(self.num_qubits))
             qc_baseline.rx(2 * m, range(self.num_qubits))
 
@@ -92,12 +84,14 @@ class QAOA:
 
         return qc, qc_baseline, remove_gates
 
-    def compile_qc(self, baseline: bool = False, opt_level: int = 2) -> QuantumCircuit:
+    def compile_qc(self, baseline: bool = False, opt_level: int = 3) -> QuantumCircuit:
+        """Compiles the circuit"""
         if baseline:
             return transpile(self.qc_baseline, backend=self.backend, optimization_level=opt_level, seed_transpiler=42)
         return transpile(self.qc, backend=self.backend, optimization_level=opt_level, seed_transpiler=42)
 
     def get_to_be_removed_gate_indices(self) -> list[int]:
+        """Returns the indices of the gates to be removed"""
         indices_parameterized_gates = []
         for i, gate in enumerate(self.qc_compiled._data):
             if (
@@ -116,15 +110,16 @@ class QAOA:
         return indices_to_be_removed_parameterized_gates
 
     def check_gates(self, qc: QuantumCircuit, optimize_swaps: bool = True) -> QuantumCircuit:
+        """Removes the gates to be checked from the circuit at online time"""
         offset = 0
-        for i in self.to_be_checked_gates_indices:
-            # Remove the gate from the ParameterTable
-            # del qc._parameter_table[qc._data[i - offset].operation.params[0]]._instance_ids[
-            #     (id(qc._data[i - offset].operation), 0)
-            # ]
+
+        # Iterate over all gates to be removed
+        for i in self.to_be_removed_gates_indices:
+            # Remove the gate from the ParameterTable (was needed for QCEC to work)
             del qc._parameter_table[
                 qc._data[i - offset].operation.params[0]
             ]  # remove the Parameter from the ParameterTable for the specific parameter. The table just contains one entry
+            # Delete the gate and optionally the enclosing CX gates
             if (
                 optimize_swaps
                 and qc._data[i - offset - 1].operation.name == "cx"
@@ -140,12 +135,16 @@ class QAOA:
         return qc
 
 
-def reduce_swaps(qc: QuantumCircuit) -> QuantumCircuit:
-    transpile_passes = [
-        CommutativeCancellation(),
-        CommutativeInverseCancellation(),
-        CXCancellation(),
-        Collect2qBlocks(),
-        ConsolidateBlocks(),
-    ]
-    return PassManager(transpile_passes).run(qc).decompose()
+def get_backend(num_qubits: int) -> FakeBackend:
+    FakeManila().configuration()
+    montreal_config = FakeMontreal().configuration()
+    washington_config = FakeWashington().configuration()
+    manila_config = FakeManila().configuration()
+    FakeManila().configuration()
+    if num_qubits <= manila_config.n_qubits:
+        return FakeManila()
+    if num_qubits <= montreal_config.n_qubits:
+        return FakeMontreal()
+    if num_qubits <= washington_config.n_qubits:
+        return FakeWashington()
+    return None
