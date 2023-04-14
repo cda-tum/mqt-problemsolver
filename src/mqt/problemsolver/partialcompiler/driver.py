8d718799ede9dc86a74cf8b826f0aa24f23ed975
Nils Quetschlich <Nils.Quetschlich@tum.de>
2023-04-14 18:33:19 +0200

    🎨 adjusted evaluations

--- src/mqt/problemsolver/partialcompiler/driver.py
+++ src/mqt/problemsolver/partialcompiler/driver.py
@@ -3,7 +3,7 @@ from joblib import Parallel, delayed
 from mqt.problemsolver.partialcompiler.evaluator import evaluate_QAOA
 
 
-def eval_all_instances(min_qubits: int = 3, max_qubits: int = 80, stepsize: int = 10) -> None:
+def eval_all_instances_QAOA(min_qubits: int = 3, max_qubits: int = 80, stepsize: int = 10) -> None:
     res_csv = []
     results = Parallel(n_jobs=-1, verbose=3)(
         delayed(evaluate_QAOA)(i, 3, j, k)
@@ -16,7 +16,24 @@ def eval_all_instances(min_qubits: int = 3, max_qubits: int = 80, stepsize: int 
     for res in results:
         res_csv.append(list(res.values()))
     np.savetxt(
-        "res.csv",
+        "res_qaoa.csv",
+        res_csv,
+        delimiter=",",
+        fmt="%s",
+    )
+
+
+def eval_all_instances_Satellite(min_qubits: int = 3, max_qubits: int = 80, stepsize: int = 10) -> None:
+    res_csv = []
+    results = Parallel(n_jobs=-1, verbose=3)(
+        delayed(evaluate_QAOA)(i, 3, 0.4, 1, True) for i in range(min_qubits, max_qubits, stepsize)
+    )
+
+    res_csv.append(list(results[0].keys()))
+    for res in results:
+        res_csv.append(list(res.values()))
+    np.savetxt(
+        "res_satellite.csv",
         res_csv,
         delimiter=",",
         fmt="%s",
