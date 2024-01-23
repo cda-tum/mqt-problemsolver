e6dbc319036cba5f9a276c468d5f7abc0f6cc88c
Damian Rovara <damian.rovara@tum.de>
2024-01-23 16:25:59 +0100

    fix: :bug: Fixed incorrect test file
    The test file test_cost_functions_one_hot was not updated to the new version yet

--- QUBOMaker/tests/test_cost_functions_one_hot.py
+++ QUBOMaker/tests/test_cost_functions_one_hot.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import mqt.pathfinder.cost_functions as cf
-import mqt.pathfinder.graph as graph
+import mqt.qubomaker.pathfinder as pf
+import mqt.qubomaker.pathfinder.cost_functions as cf
+from mqt.qubomaker import Graph
 
 if TYPE_CHECKING:
     import sympy as sp
 
-TEST_GRAPH = graph.Graph(
+TEST_GRAPH = Graph(
     5,
     [
         (1, 2, 5),
@@ -39,16 +40,16 @@ def paths_to_assignment(paths: list[list[int]], n_vertices: int, max_path_length
     return dict(result)
 
 
-def assert_returns_0(cost_function: cf.CostFunction, path: dict[sp.Expr, int]) -> None:
-    settings = cf.PathFindingQUBOGeneratorSettings(cf.EncodingType.ONE_HOT, 1, TEST_GRAPH.n_vertices, False)
+def assert_returns_0(cost_function: pf.CostFunction, path: dict[sp.Expr, int]) -> None:
+    settings = pf.PathFindingQUBOGeneratorSettings(pf.EncodingType.ONE_HOT, 1, TEST_GRAPH.n_vertices, False)
     formula = cost_function.get_formula(TEST_GRAPH, settings)
     print(formula)
     value = formula.subs(path).doit()  # type: ignore[no-untyped-call]
     assert value == 0
 
 
-def assert_returns_gt_0(cost_function: cf.CostFunction, path: dict[sp.Expr, int]) -> None:
-    settings = cf.PathFindingQUBOGeneratorSettings(cf.EncodingType.ONE_HOT, 1, TEST_GRAPH.n_vertices, False)
+def assert_returns_gt_0(cost_function: pf.CostFunction, path: dict[sp.Expr, int]) -> None:
+    settings = pf.PathFindingQUBOGeneratorSettings(pf.EncodingType.ONE_HOT, 1, TEST_GRAPH.n_vertices, False)
     formula = cost_function.get_formula(TEST_GRAPH, settings)
     value = formula.subs(path).doit()  # type: ignore[no-untyped-call]
     assert value > 0
@@ -58,13 +59,13 @@ def test_path_position_is() -> None:
     encoding_A = paths_to_assignment([[1, 3, 4, 2, 5]], TEST_GRAPH.n_vertices, TEST_GRAPH.n_vertices)
     encoding_B = paths_to_assignment([[1, 3, 4], [2, 5]], TEST_GRAPH.n_vertices, TEST_GRAPH.n_vertices)
 
-    assert_returns_0(cf.PathPositionIs(2, [3], 1), encoding_A)
-    assert_returns_0(cf.PathPositionIs(2, [2, 3], 1), encoding_A)
-    assert_returns_0(cf.PathPositionIs(2, [1, 5], 2), encoding_B)
+    assert_returns_0(pf.PathPositionIs(2, [3], 1), encoding_A)
+    assert_returns_0(pf.PathPositionIs(2, [2, 3], 1), encoding_A)
+    assert_returns_0(pf.PathPositionIs(2, [1, 5], 2), encoding_B)
 
-    assert_returns_gt_0(cf.PathPositionIs(1, [4], 1), encoding_A)
-    assert_returns_gt_0(cf.PathPositionIs(1, [4, 3, 2], 1), encoding_A)
-    assert_returns_gt_0(cf.PathPositionIs(1, [1], 2), encoding_B)
+    assert_returns_gt_0(pf.PathPositionIs(1, [4], 1), encoding_A)
+    assert_returns_gt_0(pf.PathPositionIs(1, [4, 3, 2], 1), encoding_A)
+    assert_returns_gt_0(pf.PathPositionIs(1, [1], 2), encoding_B)
 
 
 # TODO test remaining ones
