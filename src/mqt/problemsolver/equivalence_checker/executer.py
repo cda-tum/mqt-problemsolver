b1aaf14691bd133ea894439ed4d3cd502b43d1f1
tobi-forster <t.forster@tum.de>
2024-08-14 09:54:22 +0200

    Added function to export results as .csv

--- src/mqt/problemsolver/equivalence_checker/executer.py
+++ src/mqt/problemsolver/equivalence_checker/executer.py
@@ -4,6 +4,8 @@ import string
 
 import numpy as np
 
+import pandas as pd
+
 # from mqt.problemsolver.equivalence_checker import sampler
 from qiskit import QuantumCircuit
 from qiskit.circuit.library import GroverOperator, PhaseOracle
@@ -15,13 +17,13 @@ sim_counts = AerSimulator(method="statevector")
 alphabet = list(string.ascii_lowercase)
 
 
-def create_condition_string(num_qubits: int, num_counter_examples: int) -> tuple[str, list[str]]:
+def create_condition_string(num_bits: int, num_counter_examples: int) -> tuple[str, list[str]]:
     """
     Creates a string to simulate a miter out of bitstring combinations (e.g. '0000' -> 'a & b & c & d')
 
     Parameters
     ----------
-    num_qubits : int
+    num_bits : int
         Number of input bits
     num_counter_examples : int
         Number of counter examples
@@ -34,19 +36,19 @@ def create_condition_string(num_qubits: int, num_counter_examples: int) -> tuple
         The corresponding bitstrings to res_string (e.g. counter_examples is ['0000'] for res_string 'a & b & c & d')
     """
 
-    if num_qubits < 0 or num_counter_examples < 0:
+    if num_bits < 0 or num_counter_examples < 0:
         raise ValueError
 
     counter_examples: list[str] = []
     if num_counter_examples == 0:
-        res, _ = create_condition_string(num_qubits, 1)
+        res, _ = create_condition_string(num_bits, 1)
         res += " & a"
         return res, counter_examples
     res_string: str = ""
     counter_examples = []
     for num in range(num_counter_examples):
-        bitstring = list(str(format(num, f"0{num_qubits}b")))[::-1]
-        counter_examples.append(str(format(num, f"0{num_qubits}b")))
+        bitstring = list(str(format(num, f"0{num_bits}b")))[::-1]
+        counter_examples.append(str(format(num, f"0{num_bits}b")))
         for i, char in enumerate(bitstring):
             if char == "0" and i == 0:
                 bitstring[i] = "~" + alphabet[i]
@@ -95,11 +97,11 @@ def sampler(
         oracle = PhaseOracle(miter)
 
         operator = GroverOperator(oracle).decompose()
-        num_qubits = operator.num_qubits
-        total_num_combinations = 2**num_qubits
+        num_bits = operator.num_qubits
+        total_num_combinations = 2**num_bits
 
-        qc = QuantumCircuit(num_qubits)
-        qc.h(list(range(num_qubits)))
+        qc = QuantumCircuit(num_bits)
+        qc.h(list(range(num_bits)))
         qc.compose(operator.power(iterations).decompose(), inplace=True)
         qc.measure_all()
 
@@ -115,6 +117,7 @@ def sampler(
             sorted(counts_dict.items(), key=lambda item: item[1])[::-1]
         )  # Sort state dictionary with respect to values (counts)
 
+        target_states = []
         stopping_condition = False
         for i in range(round(total_num_combinations * 0.5)):
             if (i + 1) == len(counts_list):
@@ -139,21 +142,31 @@ def sampler(
         if stopping_condition:
             break
 
+    # if sorted(target_states) == sorted(counter_examples):
+    #     return f"Correct targets found! Total number of iterations: {total_iterations}"
+    # if len(target_states) == 0:
+    #     if len(counter_examples) > 0:
+    #         return f"No targets found! Total number of iterations: {total_iterations}"
+    #     if len(counter_examples) == 0:
+    #         return f"Correct targets found (None)! Total number of iterations: {total_iterations}"
+
+    # return f"At least one wrong target found! Total number of iterations: {total_iterations}"
+
     if sorted(target_states) == sorted(counter_examples):
-        return f"Correct targets found! Total number of iterations: {total_iterations}"
+        return total_iterations
     if len(target_states) == 0:
         if len(counter_examples) > 0:
-            return f"No targets found! Total number of iterations: {total_iterations}"
+            return None
         if len(counter_examples) == 0:
-            return f"Correct targets found (None)! Total number of iterations: {total_iterations}"
+            return total_iterations
 
-    return f"At least one wrong target found! Total number of iterations: {total_iterations}"
+    return None
 
 
 def find_counter_examples(
     miter: str,
     counter_examples: list[str],
-    num_qubits: int,
+    num_bits: int,
     shots: int,
     delta: float,
 ) -> str:
@@ -164,7 +177,7 @@ def find_counter_examples(
     ----------
     miter: str
         String that contains the conditions to satisfy
-    num_qubits : int
+    num_bits : int
         Number of input bits
     shots: int
         Number of shots
@@ -183,6 +196,31 @@ def find_counter_examples(
     except AssertionError:
         print(f"Invalid delta of {delta}. It must be between 0 and 1.")
 
-    total_num_combinations = 2**num_qubits
+    total_num_combinations = 2**num_bits
     start_iterations = np.floor(np.pi / (4 * np.arcsin((1 / total_num_combinations) ** 0.5)) - 0.5).astype(int)
     return sampler(miter, counter_examples, start_iterations, shots, delta)
+
+
+def run(path, range_deltas, range_num_bits, range_fraction_counter_examples, num_runs, verbose=False):
+    data = pd.DataFrame(columns = ['Input Bits','Counter Examples'] + [delta for delta in deltas])
+    i = 0
+    for num_bits in range_num_bits:
+        for fraction_counter_examples in range_fraction_counter_examples:
+            row = [num_bits, fraction_counter_examples]
+            for delta in range_deltas:
+                if verbose:
+                    print(f"num_bits: {num_bits}, fraction_counter_examples: {fraction_counter_examples}, delta: {delta}")
+                results = []
+                for run in range(num_runs):
+                    num_counter_examples = round(fraction_counter_examples * 2**num_bits)
+                    miter, counter_examples = create_condition_string(num_bits, num_counter_examples)
+                    result = find_counter_examples(miter, counter_examples, num_bits, 8 * (2**num_bits), delta)
+                    results.append(result)
+                if None in results:
+                    row.append("-")
+                else:
+                    row.append(np.mean(results))
+            data.loc[i] = row
+            i += 1
+
+    data.to_csv(path, index=False)
