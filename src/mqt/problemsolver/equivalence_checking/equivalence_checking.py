4de52e498c5e645b5412699bc54075ab08ff8a6b
tobi-forster <t.forster@tum.de>
2024-08-23 16:26:45 +0200

    Updated tests

--- src/mqt/problemsolver/equivalence_checking/equivalence_checking.py
+++ src/mqt/problemsolver/equivalence_checking/equivalence_checking.py
@@ -57,12 +57,12 @@ def create_condition_string(num_bits: int, num_counter_examples: int) -> tuple[s
             ::-1
         ]  # e.g. ['0', '0', '0', '0'] for num = 0 and num_bits = 4
         counter_examples.append(str(format(num, f"0{num_bits}b")))  # appends ['0000'] for num = 0 and num_bits = 4
-        for i, char in enumerate(bitstring):  # the following lines add a negated letter (e.g. "~a") for
-            if char == "0" and i == 0:        # each "0" and a letter (e.g. "a") for each "1" in the bitstring list.
-                bitstring[i] = (              # If the first letter is added (so i > 0), the subsequent letters are added together
-                    "~" + alphabet[i]         # with a logical AND operator (e.g. "~a & ~b & ~c & ~d").
-                )  
-            elif char == "1" and i == 0:  
+        # The following lines add a negated letter (e.g. "~a") for each "0" and a letter (e.g. "a") for each "1" in the bitstring list.
+        # If the first letter is added (so i > 0), the subsequent letters are added together with a logical AND operator (e.g. "~a & ~b & ~c & ~d").
+        for i, char in enumerate(bitstring):
+            if char == "0" and i == 0:
+                bitstring[i] = "~" + alphabet[i]
+            elif char == "1" and i == 0:
                 bitstring[i] = alphabet[i]
             elif char == "0":
                 bitstring[i] = " & " + "~" + alphabet[i]
--- tests/test_equivalence_checking.py
+++ tests/test_equivalence_checking.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import importlib
-import string
 from typing import TYPE_CHECKING
 
 import pytest
@@ -64,3 +63,6 @@ def test_find_counter_examples() -> None:
         found_counter_examples.sort()
     counter_examples.sort()
     assert found_counter_examples == counter_examples
+
+    with pytest.raises(ValueError, match="Invalid value for delta 1.2, which must be between 0 and 1."):
+        equivalence_checking.find_counter_examples(miter=res_string, num_bits=5, shots=shots, delta=1.2)
