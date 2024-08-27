33304b92775a01676c5919d7656de7bc8a6efd6d
Nils Quetschlich <nils.quetschlich@tum.de>
2024-08-27 10:07:12 +0200

    Update resource_estimation/RE_experiments.py
    Signed-off-by: Nils Quetschlich <nils.quetschlich@tum.de>

--- resource_estimation/RE_experiments.py
+++ resource_estimation/RE_experiments.py
@@ -6,7 +6,7 @@ from qsharp.estimator import EstimatorParams, EstimatorResult, LogicalCounts, QE
 
 # For all experiments, we are using the logical resource counts as a starting
 # point.  These have been computed using the qsharp Python package (version
-# 1.6.0) for the https://aka.ms/fcidump/XVIII-cas4-fb-64e-56o Hamiltonian on
+# 1.8.0) for the https://aka.ms/fcidump/XVIII-cas4-fb-64e-56o Hamiltonian on
 # the sample
 # https://github.com/microsoft/qsharp/tree/main/samples/estimation/df-chemistry:
 #
