4fcfad22f93949561635f53dcd5401b97131cdc3
burgholzer <burgholzer@me.com>
2024-10-10 16:13:58 +0200

    ✏️ use crates-ci/typos instead of codespell

--- QUBOMaker/src/mqt/qubomaker/__init__.py
+++ QUBOMaker/src/mqt/qubomaker/__init__.py
@@ -1,6 +1,6 @@
 """A package for generating QUBO formulations automatically from a set of constraints QUBOs for different problem classes.
 
-It allows users to create a `QUBOGenerator` object, and graudally add penalty terms and constraints to it.
+It allows users to create a `QUBOGenerator` object, and gradually add penalty terms and constraints to it.
 When done, the object can be used to construct a QUBO formulation of the project on multiple granularity levels.
 
 ## Available Subpackages
