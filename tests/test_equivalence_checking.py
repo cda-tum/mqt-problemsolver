f9b7010323a8a152817a2b1c3ea221d8ff5bba4a
Nils Quetschlich <nils.quetschlich@tum.de>
2025-05-08 16:34:23 +0200

    :art: refactored tests

--- tests/test_equivalence_checking.py
+++ tests/test_equivalence_checking.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-import importlib
 from typing import TYPE_CHECKING
 
 import pytest
@@ -28,7 +27,6 @@ def test_create_condition_string() -> None:
         equivalence_checking.create_condition_string(num_bits=-5, num_counter_examples=-2)
 
 
-@pytest.mark.skipif(not importlib.util.find_spec("tweedledum"), reason="tweedledum is not installed")
 def test_try_parameter_combinations(tmp_path: Path) -> None:
     """Test the function try_parameter_combinations."""
     path_to_csv = tmp_path / "sub" / "test1.csv"
@@ -46,55 +44,40 @@ def test_try_parameter_combinations(tmp_path: Path) -> None:
     path_to_csv.parent.rmdir()
 
 
-@pytest.mark.skipif(not importlib.util.find_spec("tweedledum"), reason="tweedledum is not installed")
 @pytest.mark.parametrize(
-    ("num_bits", "num_counter_examples", "delta", "shots"),
+    ("num_bits", "num_counter_examples", "get_counter_examples", "expected_num_iters"),
     [
-        (6, 3, 0.7, 512),
-        (6, 32, 0.7, 512),
-        (6, 57, 0.7, 512),
-        (6, 0, 0.7, 512),
+        (6, 3, True, 5),
+        (6, 32, True, None),
+        (6, 57, False, None),
+        (6, 0, False, None),
     ],
 )
-def test_find_counter_examples(num_bits: int, num_counter_examples: int, delta: float, shots: int) -> None:
+def test_determine_number_grover_iterations(
+    num_bits: int, num_counter_examples: int, get_counter_examples: bool, expected_num_iters: None | int
+) -> None:
     """Test the function find_counter_examples."""
 
     res_string, predetermined_counter_examples = equivalence_checking.create_condition_string(
         num_bits=num_bits, num_counter_examples=num_counter_examples
     )
 
-    if num_counter_examples == 32:
-        found_counter_examples = equivalence_checking.find_counter_examples(
-            miter=res_string,
-            num_bits=num_bits,
-            shots=shots,
-            delta=delta,
-            predetermined_counter_examples=predetermined_counter_examples,
-        )
-        assert found_counter_examples is None
+    result = equivalence_checking.find_counter_examples(
+        miter=res_string,
+        num_bits=num_bits,
+        shots=512,
+        delta=0.7,
+        predetermined_counter_examples=predetermined_counter_examples if get_counter_examples else None,
+    )
 
+    if get_counter_examples:
+        assert result is expected_num_iters
     else:
-        found_counter_examples = equivalence_checking.find_counter_examples(
-            miter=res_string, num_bits=num_bits, shots=shots, delta=delta
-        )
+        assert isinstance(result, list)
+        assert result.sort() == predetermined_counter_examples.sort()
 
-        if isinstance(found_counter_examples, list):
-            found_counter_examples.sort()
-        predetermined_counter_examples.sort()
 
-        assert found_counter_examples == predetermined_counter_examples
-
-    if num_counter_examples == 3:
-        number_iterations = equivalence_checking.find_counter_examples(
-            miter=res_string,
-            num_bits=num_bits,
-            shots=shots,
-            delta=delta,
-            predetermined_counter_examples=predetermined_counter_examples,
-        )
-
-        # 5 iterations were determined by multiple experiments in the case of 6 bits and 3 counter examples
-        assert number_iterations == 5
-
+def test_faulty_delta_value() -> None:
+    """Test the function find_counter_examples with a faulty delta value."""
     with pytest.raises(ValueError, match="Invalid value for delta 1.2, which must be between 0 and 1."):
-        equivalence_checking.find_counter_examples(miter=res_string, num_bits=5, shots=shots, delta=1.2)
+        equivalence_checking.find_counter_examples(miter="", num_bits=5, shots=512, delta=1.2)
