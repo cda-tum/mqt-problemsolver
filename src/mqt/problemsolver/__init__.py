f571aef234e1f60b64fc4fcb5bb2fe7080f3a02a
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-08-27 18:07:10 +0200

    Add _version.pyi

--- src/mqt/problemsolver/__init__.py
+++ src/mqt/problemsolver/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
 from ._version import version as __version__
+from ._version import version_tuple as version_info
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "version_info"]
